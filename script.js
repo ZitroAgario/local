fe427b34384410a2868dd969f0c245f08721b14a
ZitroAgario <crspd@i.ua>
2017-06-12 15:28:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function init(){
 var dm=document;
 dm.qr=document.querySelector;
 var cv = dm.qr('#cvs'); 
@@ -6,9 +7,12 @@ var p=Math.PI;
 img=dm.qr('#img2');
 var pat=c.createPattern(img,'no-repeat')
 c.fillStyle=pat;
+var idt=c.getImageData(0,0,150,150);
 c.beginPath();
 c.arc(75,75,75,1,5*p,0,5*p);
 c.fill();
-var idt=c.getImageData(0,0,150,150);
-c.putImageData(idt,cv.width/2,cv.height/2)
-//dm.innerHTML+=cv.toDataURL();
+var idt2=c.getImageData(0,0,150,150);
+c.putImageData(idt,0,0)
+c.putImageData(idt2,cv.width/2-75, cv.height/2-75);
+dm.innerHTML+=cv.toDataURL();
+}
